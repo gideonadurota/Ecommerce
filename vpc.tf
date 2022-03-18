4464a506ed37482cfbaeaa51ecf76534fe41a1a7
Gideon Adurota <gideonadurota@outlook.com>
2022-03-18 20:43:43 +0000

    Add the internet gateway

--- vpc.tf
+++ vpc.tf
@@ -5,6 +5,25 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "VPC"   
+    Name = "Test VPC"   
+  }
+}
+
+#Create internet gateway
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "Test Internet Gateway"
+  }
+}
+
+#Create public subnet 1
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = 
+
+  tags = {
+    Name = "Public subnet 1"
   }
 }
