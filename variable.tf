4cf453925080b059e629e9ea81e334c992a996f6
Gideon Adurota <gideonadurota@outlook.com>
2022-03-18 20:46:37 +0000

    Add the public subnet 1

--- variable.tf
+++ variable.tf
@@ -2,3 +2,8 @@ variable "vpc-cidr" {
     default = "10.0.0.0/16"
     description = "This is the cidr for the vpc"
 }
+
+variable "public-subnet-1-cidr" {
+    default = "10.0.0.0/24"
+    description = "This is the cidr for the public subnet 1"
+}
