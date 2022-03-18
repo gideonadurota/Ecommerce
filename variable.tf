bf471372a70677e3c4743440e1575634cc87dcfb
Gideon Adurota <gideonadurota@outlook.com>
2022-03-18 20:38:42 +0000

    Add the vpc block and variable

---
+++ variable.tf
@@ -0,0 +1,4 @@
+variable "vpc-cidr" {
+    default = "10.0.0.0/16"
+    description = "This is the cidr for the vpc"
+}
---
+++ vpc.tf
@@ -0,0 +1,10 @@
+#Create VPC
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc-cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "VPC"   
+  }
+}
