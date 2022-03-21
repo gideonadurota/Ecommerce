c9ba2777952bd4aba9a65b90e67389a5740dfc93
Gideon Adurota <gideonadurota@outlook.com>
2022-03-21 08:55:45 +0000

    add the private subnet 1

--- vpc.tf
+++ vpc.tf
@@ -72,9 +72,10 @@ resource "aws_subnet" "private-subnet-1" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.private-subnet-1-cidr
   availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
-    Name = "private subnet 1"
+    Name = "private subnet 1 | App tier"
   }
 }
+
