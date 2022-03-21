36bc99648cf4aa3f1bca888d273fb27db63df70d
Gideon Adurota <gideonadurota@outlook.com>
2022-03-21 08:45:45 +0000

    add the private subnet 1

--- variable.tf
+++ variable.tf
@@ -1,14 +1,23 @@
 variable "vpc-cidr" {
     default = "10.0.0.0/16"
     description = "This is the cidr for the vpc"
+    type = string
 }
 
 variable "public-subnet-1-cidr" {
     default = "10.0.0.0/24"
     description = "This is the cidr for the public subnet 1"
+    type = string
 }
 
 variable "public-subnet-2-cidr" {
     default = "10.0.1.0/24"
     description = "This is the cidr for the public subnet 2"
+    type = string
+}
+
+variable "private-subnet-1-cidr" {
+    default = "10.0.2.0/24"
+    description = "This is the cidr for the private subnet 1"
+    type = string
 }
--- vpc.tf
+++ vpc.tf
@@ -67,3 +67,14 @@ resource "aws_route_table_association" "public-subnet-2-public-route-table-assoc
   subnet_id      = aws_subnet.public-subnet-2.id
   route_table_id = aws_route_table.public-route-table.id
 }
+
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.private-subnet-1-cidr
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "private subnet 1"
+  }
+}
