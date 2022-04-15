cc2a92a0e2e3ec6866629060feb0cf50f27f95b6
Gideon Adurota <gideonadurota@outlook.com>
2022-04-15 01:36:33 +0000

    create route for nat gateway 1

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -39,3 +39,17 @@ resource "aws_nat_gateway" "nat-gateway-2" {
   }
 
 }
+
+#Create Private route table 1 and add route through Nat gateway 1
+resource "aws_route_table" "private-route-table-1" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gateway-1.id
+  }
+
+  tags = {
+      Name = "private route table 1"
+  }
+}
