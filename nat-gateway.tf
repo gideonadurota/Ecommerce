335f929031e8a778d69ea682327fe6c7aba42f48
Gideon Adurota <gideonadurota@outlook.com>
2022-04-15 01:15:10 +0000

    create nat gateway 2

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -27,7 +27,15 @@ resource "aws_nat_gateway" "nat-gateway-1" {
     Name = "nat-gateway 1"
   }
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.example]
+}
+
+#Create NAT Gateway 2
+resource "aws_nat_gateway" "nat-gateway-2" {
+  allocation_id = aws_eip.eip-for-nat-gateway-2.id
+  subnet_id     = aws_subnet.public-subnet-2.id
+
+  tags = {
+    Name = "nat-gateway 2"
+  }
+
 }
